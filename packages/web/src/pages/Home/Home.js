d157fe29ab0d6e6be4ca3f3ebc7dcfea5b7244f9
iarrieta90 <irati_arrieta90@hotmail.com>
2021-05-19 11:07:40 +0200

    feat: post component basic layout

--- packages/web/src/pages/Home/Home.js
+++ packages/web/src/pages/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Main from "../../layout/Main";
+import Post from "../../components/Post";
 
 function Home() {
   return (
     <Main>
-      <p>Hello from home</p>
+      <Post />
     </Main>
   );
 }
